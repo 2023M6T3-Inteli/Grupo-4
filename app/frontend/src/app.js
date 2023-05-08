aaeba719febd8e3f2f0c1ed01717d02c23beb592
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-05-08 17:45:53 -0300

    react app template created

--- app/frontend/src/app.js
+++ app/frontend/src/app.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NewPage from './pages/newpage';
-// Import other necessary components
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Switch>
-        {/* Add your other routes here */}
-        <Route path="/new-page" component={NewPage} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
