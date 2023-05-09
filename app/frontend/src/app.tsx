c31fab217044d1c76f36a006adeb4a85522596e1
naoassisto <99260189+naoassisto@users.noreply.github.com>
2023-05-09 17:09:19 -0300

    New Project

---
+++ app/frontend/src/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NewProject from './pages/NewProject';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+
+      <NewProject/>
+      
+    </div>
+  );
+};
+
+export default App;
