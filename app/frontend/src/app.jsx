bbae96b9710af22da236c8900360a4d06d8687cb
FelipeSantosInteli <felipe.santos@sou.inteli.edu.br>
2023-05-10 12:06:30 -0300

    app.jsx

---
+++ app/frontend/src/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+// Import other necessary components
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Add your other routes here */}
+        <Route path="/" element={<div>
+          LOGIN
+        </div>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
