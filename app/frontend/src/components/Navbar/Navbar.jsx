f04afd94482d006f671b7e7237625c37a2c92518
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-05-10 12:50:56 -0300

    Update Navbar.jsx

--- app/frontend/src/components/Navbar/Navbar.jsx
+++ app/frontend/src/components/Navbar/Navbar.jsx
@@ -2,22 +2,23 @@
 
 const Navbar = (props) => {
   return (<>
-      <nav>
-        <button>
-          <i class="fa-solid fa-bars"></i>
-        </button>
-        <button>
-          <i class="fa-solid fa-bell"></i>
-        </button>
-      </nav>
-      <div>
-        <ul>
-          <li>
-            <i class="fa-solid fa-house"></i>
-          </li>
-        </ul>
-      </div>
-      
+
+    <nav>
+    <button>
+        <i class="fa-solid fa-bars"></i>
+    </button>
+    <button>
+        <i class="fa-solid fa-bell"></i>
+    </button>
+    </nav>
+    <div>
+    <ul>
+        <li>
+        <i class="fa-solid fa-house"></i>
+        </li>
+    </ul>
+    </div>
+
 </>);
 };
 
