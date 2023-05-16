d25009ea679ca90a6fb7522afb0ad8dacdff4507
naoassisto <99260189+naoassisto@users.noreply.github.com>
2023-05-16 16:55:23 -0300

    update prisma

--- app/backend/app.js
+++ app/backend/app.js
@@ -41,6 +41,4 @@ const projectRouter = require('./routes/projectRoutes');
 
 app.use('/v1/project', projectRouter);
 
-app.use((req, res, next) => {
-    res.status(404).send({ error: 'Not found', status: 404, url: req.url })
-})
+
