9ecd8b0558d162ea26ddda9056b61cb6b92a56f9
naoassisto <99260189+naoassisto@users.noreply.github.com>
2023-05-25 16:37:42 -0300

    Documentação Swagger

--- app/backend/routes/content.js
+++ app/backend/routes/content.js
@@ -7,30 +7,68 @@ const contentController = require('../controllers/content');
 //Middlewares
 const unsureAuthenticated = require('../middlewares/unsureAuthenticated');
 
-router.post(
-  "/create",
-  [body("title", "Titulo é necessário").exists({ checkFalsy: true })],
-  [body("description", "Descrição é necessária").exists({ checkFalsy: true })],
-  [body("tags", "Tags são necessárias").exists({ checkFalsy: true })],
-  // [body("ownerId", "Id do Owner é necessário").exists({ checkFalsy: true })],
-  unsureAuthenticated.unsureAuthenticated,
-  contentController.Create
-);
+/**
+ * @swagger
+ * /v1/content/create:
+ *   post:
+ *     description: Create new content
+ *     parameters:
+ *       - name: title
+ *         description: Content title
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: description
+ *         description: Content description
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: tags
+ *         description: Content tags
+ *         in: body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.post
+    ("/create", 
+    [body("title", "Titulo é necessário").exists({ checkFalsy: true })], 
+    [body("description", "Descrição é necessária").exists({ checkFalsy: true })], 
+    [body("tags", "Tags são necessárias").exists({ checkFalsy: true })], 
+    unsureAuthenticated.unsureAuthenticated, 
+    contentController.Create);
 
-router.get(
-  "/getContent/:id",
-  [param("id", "Id do projeto é necessário").exists({ checkFalsy: true })],
-  // unsureAuthenticated.unsureAuthenticated,
-  contentController.GetContentByID
-);
-router.get(
-  "/getContent",
-  // unsureAuthenticated.unsureAuthenticated,
-  contentController.GetAllContent
-);
+/**
+ * @swagger
+ * /v1/content/getContent/{id}:
+ *   get:
+ *     description: Get content by ID
+ *     parameters:
+ *       - name: id
+ *         description: Content ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.get
+    ("/getContent/:id", 
+    [param("id", "Id do conteudo é necessário").exists({ checkFalsy: true })],
+     contentController.GetContentByID);
 
+/**
+ * @swagger
+ * /v1/content/getContent:
+ *   get:
+ *     description: Get all content
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.get("/getContent", contentController.GetAllContent);
 
 module.exports = router;
-
-
-
--- app/backend/routes/user.js
+++ app/backend/routes/user.js
@@ -7,50 +7,148 @@ const userController = require("../controllers/user");
 //Middlewares
 const unsureAuthenticated = require("../middlewares/unsureAuthenticated");
 
-router.post(
-    "/register",
-    [body("email", "Email é necessário").exists({ checkFalsy: true })],
-    [body("password", "Senha é necessária").exists({ checkFalsy: true })],
-    [body("name", "Nome é necessária").exists({ checkFalsy: true })],
-    [body("area", "Area é necessária").exists({ checkFalsy: true })],
-    [body("tags", "Tags é necessária").exists({ checkFalsy: true })],
-    userController.Create
-);
+/**
+ * @swagger
+ * /v1/user:
+ *   get:
+ *     description: Get the authenticated user
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.get("/", unsureAuthenticated.unsureAuthenticated, userController.GetUserCalling);
 
-router.post(
-    "/auth",
-    [body("email", "Email é necessário").exists({ checkFalsy: true })],
-    [body("password", "Senha é necessária").exists({ checkFalsy: true })],
-    userController.Auth
-);
+/**
+ * @swagger
+ * /v1/user/{id}:
+ *   get:
+ *     description: Get a user by ID
+ *     parameters:
+ *       - name: id
+ *         description: User ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.get
+        ("/getUserByID/:id", 
+        unsureAuthenticated.unsureAuthenticated, 
+        userController.GetUser);
 
-router.put(
-    "/update/:id",
-    [param("id", "ID é necessário").exists({ checkFalsy: true })],
-    unsureAuthenticated.unsureAuthenticated,
-    userController.Update
-);
+/**
+ * @swagger
+ * /v1/user/register:
+ *   post:
+ *     description: Register a new user
+ *     parameters:
+ *       - name: email
+ *         description: User's email
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: name
+ *         description: User's name
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: area
+ *         description: User's area
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: tags
+ *         description: User's tags
+ *         in: body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.post
+        ("/register", 
+        [body("email", "Email é necessário").exists({ checkFalsy: true })], 
+        [body("password", "Senha é necessária").exists({ checkFalsy: true })], 
+        [body("name", "Nome é necessária").exists({ checkFalsy: true })], 
+        [body("area", "Area é necessária").exists({ checkFalsy: true })], 
+        [body("tags", "Tags é necessária").exists({ checkFalsy: true })],
+         userController.Create);
 
-router.delete(
-    "/delete/:id",
-    [param("id", "ID é necessário").exists({ checkFalsy: true })],
-    unsureAuthenticated.unsureAuthenticated,
-    userController.Delete
-);
+/**
+ * @swagger
+ * /v1/user/auth:
+ *   post:
+ *     description: Authenticate a user
+ *     parameters:
+ *       - name: email
+ *         description: User's email
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password
+ *         in: body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.post
+        ("/auth", 
+        [body("email", "Email é necessário").exists({ checkFalsy: true })], 
+        [body("password", "Senha é necessária").exists({ checkFalsy: true })], 
+        userController.Auth);
 
-router.get(
-    "/getUserByID/:id",
-    [param("id", "ID é necessário").exists({ checkFalsy: true })],
-    unsureAuthenticated.unsureAuthenticated,
-    userController.GetUser
-);
+/**
+ * @swagger
+ * /v1/user/update/{id}:
+ *   put:
+ *     description: Update a user by ID
+ *     parameters:
+ *       - name: id
+ *         description: User ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description:Success
+ */
+router.put
+        ("/update/:id", 
+        [param("id", "ID é necessário").exists({ checkFalsy: true })],
+         unsureAuthenticated.unsureAuthenticated, 
+         userController.Update);
 
-router.get(
-    "/",
-    unsureAuthenticated.unsureAuthenticated,
-    userController.GetUserCalling
-);
-
+/**
+ * @swagger
+ * /v1/user/delete/{id}:
+ *   delete:
+ *     description: Delete a user by ID
+ *     parameters:
+ *       - name: id
+ *         description: User ID
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+router.delete
+        ("/delete/:id", 
+        [param("id", "ID é necessário").exists({ checkFalsy: true })], 
+        unsureAuthenticated.unsureAuthenticated, 
+        userController.Delete);
 
 //Exporta o ROUTER
 module.exports = router;
